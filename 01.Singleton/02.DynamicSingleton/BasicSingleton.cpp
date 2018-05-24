16604ca72ceabaceda4956ee38f3f881aabb1bd8
KwonHo.Jeong <33522408+JeongKwonHo@users.noreply.github.com>
2018-05-24 14:17:41 +0900

    Add files via upload

---
+++ 01.Singleton/02.DynamicSingleton/BasicSingleton.cpp
@@ -0,0 +1,51 @@
+/**
+	@file	Singleton.cpp
+	@date	2018-5-24
+	@author	JeongKwonHo(jkh2802@gmail.com)
+	@brief	Dynamic Singleton using atexit
+	
+*/
+
+#include <iostream>
+
+class Singleton
+{
+private:
+	static Singleton* instance;
+
+private:
+	Singleton() {};
+	Singleton(const Singleton& other);
+	~Singleton() {};
+	
+public:
+	static Singleton* getInstance()
+	{
+		if (!instance) {
+			instance = new Singleton;
+			atexit(release_instance);
+		}
+		return instance;
+	}
+	static void release_instance() {
+		if (instance) {
+			delete instance;
+			instance = nullptr;
+			
+		}
+	}
+};
+
+Singleton* Singleton::instance = nullptr;
+
+int main()
+{
+	Singleton* s = Singleton::getInstance();
+	Singleton* r = Singleton::getInstance();
+
+	std::cout << s << std::endl;
+	std::cout << r << std::endl;
+
+
+	return 0;
+}
